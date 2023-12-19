03e0214a5a717150a6f7bd39659e92653f2577a1
Paulo Willian <paulowos@gmail.com>
2023-12-19 11:14:19 -0300

    feat: initial config

---
+++ Model/Talk.cs
@@ -0,0 +1,7 @@
+namespace TalkerManager.Model;
+
+public class Talk
+{
+    public DateTime WatchedAt { get; set; }
+    public int Rate { get; set; }
+}
---
+++ Model/Talker.cs
@@ -0,0 +1,9 @@
+namespace TalkerManager.Model;
+
+public class Talker
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+    public Talk? Talk { get; set; }
+}
